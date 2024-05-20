e380f346ac16b4e977ce998d98f772ae718d46d1
Pete Cheslock <petecheslock@gmail.com>
2024-05-20 13:45:20 -0400

    Add tests

---
+++ src/test/java/org/springframework/samples/petclinic/security/LoginFunctionalityTests.java
@@ -0,0 +1,47 @@
+package org.springframework.samples.petclinic.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginFunctionalityTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void testSuccessfulLogin() {
+		String url = "http://localhost:" + port + "/login";
+		ResponseEntity<String> response = restTemplate.withBasicAuth("user", "password").getForEntity(url, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("Login");
+	}
+
+	@Test
+	void testLoginRedirect() {
+		String url = "http://localhost:" + port + "/";
+		ResponseEntity<String> response = restTemplate.withBasicAuth("user", "password").getForEntity(url, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("Login");
+	}
+
+	@Test
+	void testLoginFailure() {
+		String url = "http://localhost:" + port + "/login";
+		ResponseEntity<String> response = restTemplate.withBasicAuth("wronguser", "wrongpassword").getForEntity(url, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+	}
+}
